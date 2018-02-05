697bed937872b7cb6b9a3ecc2c3c857929f5990f
Tudor <tudor.nimara@gmail.com>
2018-02-05 20:18:50 +0200

    Basic DB intregration test

--- app/src/main/java/com/nullniche/legumedelaradu/MainActivity.java
+++ app/src/main/java/com/nullniche/legumedelaradu/MainActivity.java
@@ -2,6 +2,10 @@ package com.nullniche.legumedelaradu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void SendClassToDB(View view){
+        //send something to db
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
